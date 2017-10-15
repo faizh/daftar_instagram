79a3d467586cee2afa5da969cb7c1189cfbc9128
faizh <30363360+faizh@users.noreply.github.com>
2017-10-15 18:42:05 +0700

    Add files via upload

---
+++ XI_RPL_2_14/view.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Sukses !!</title>
+<link rel="stylesheet" href="font-awesome/css/font-awesome.css">
+<link rel="stylesheet" href="style2.css">
+</head>
+<body>
+<div id="content">
+<div class="judul"><p align="center"><font size="5px"><b>BERHASIL !!</b></p></font></div>
+<hr>
+<div class="penerang">Selamat, Anda telah terdaftar dalam Instagram dengan biodata berikut</p></div>
+<div class="table"><table>
+<tr><td>Nomor Ponsel atau Email</td> <td>:</td> <td><?php echo $_POST["nomor"];?></td></tr>
+<tr><td>Nama Lengkap</td> <td>:</td> <td><?php echo $_POST["lengkap"];?></td></tr>
+<tr><td>Nama Pengguna</td> <td>:</td> <td><?php echo $_POST["pengguna"];?></td></tr>
+<tr><td>Kata Sandi</td> <td>:</td> <td><?php echo $_POST["sandi"];?></td></tr>
+</table></p></div>
+<hr>
+<div class="button">
+<p align="center"><button class="btn" type="submit"><a href="index.php"><b>Ulang</b></a></button></p>
+</div>
+</div>
+</body>
+</html>
+<!--Faiz Hermawan->
