1286bf0da23e252ff68ea9eacb78c47468eb98ab
faizh <30363360+faizh@users.noreply.github.com>
2017-10-22 18:59:56 +0700

    Update hasil.php

--- XI_RPL_2_14/hasil.php
+++ XI_RPL_2_14/hasil.php
@@ -6,21 +6,35 @@
 <link rel="stylesheet" href="style2.css">
 </head>
 <body>
-<div id="content">
-<div class="judul"><p align="center"><font size="5px"><b>BERHASIL !!</b></p></font></div>
-<hr>
-<div class="penerang">Selamat, Anda telah terdaftar dalam Instagram dengan biodata berikut</p></div>
-<div class="table"><table>
-<tr><td>Nomor Ponsel atau Email</td> <td>:</td> <td><?php echo $_POST["nomor"];?></td></tr>
-<tr><td>Nama Lengkap</td> <td>:</td> <td><?php echo $_POST["lengkap"];?></td></tr>
-<tr><td>Nama Pengguna</td> <td>:</td> <td><?php echo $_POST["pengguna"];?></td></tr>
-<tr><td>Kata Sandi</td> <td>:</td> <td><?php echo $_POST["sandi"];?></td></tr>
-</table></p></div>
-<hr>
-<div class="button">
-<p align="center"><button class="btn" type="submit"><a href="form.php"><b>Ulang</b></a></button></p>
-</div>
-</div>
+	<div id="content">
+		<div class="judul"><p align="center"><font size="5px"><b>BERHASIL !!</b></p></font>
+		</div>
+		<hr>
+		<div class="penerang">Selamat, Anda telah terdaftar dalam Instagram dengan biodata berikut</p>
+		</div>
+		<div class="table">
+		<?php
+		$conn=mysql_connect("localhost", "root", "");
+		mysql_select_db("dbinstagram");
+		$email=$_POST['email'];
+		$nama=$_POST['nama'];
+		$username=$_POST['username'];
+		$sandi=$_POST['sandi'];
+		$sql="insert into tbdaftar (email, nama, username, sandi) values ('$email', '$nama', '$username', '$sandi')";
+		mysql_query($sql);
+		echo "<table>";
+		echo "<tr><td>Nomor Ponsel atau Email</td> <td>:</td> <td>$email</td></tr>";
+		echo "<tr><td>Nama Lengkap</td> <td>:</td> <td>$nama</td></tr>";
+		echo "<tr><td>Nama Pengguna</td> <td>:</td> <td>$username</td></tr>";
+		echo "<tr><td>Kata Sandi</td> <td>:</td> <td>$sandi</td></tr>";
+		echo "</table>";
+		?>
+		</div>
+		<hr>
+		<div class="button">
+		<p align="center"><button class="btn" type="submit"><a href="index.php"><b>Ulang</b></a></button></p>
+		</div>
+	</div>
 </body>
 </html>
 <!--Faiz Hermawan->
